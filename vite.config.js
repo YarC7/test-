4cd1a44127e39191012b3e80a21f7f4bfb933d74
cankkun <nguyenduccanh177@gmail.com>
2025-01-09 12:51:59 +0700

    conf:configure redirect ssr

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-
+import { ssr } from "vite-plugin-ssr/plugin";
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    ssr({
+      // Permanent redirections (HTTP status code 301)
+      redirects: {
+        "/about": "/about",
+      },
+    }),
+  ],
   server: {
     historyApiFallback: {
       index: "/index.html",
